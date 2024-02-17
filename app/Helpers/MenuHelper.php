2d0121bbe98ccdd2788836e914d32276d02f96ee
unknown <mjumain11@gmail.com>
2024-02-17 12:11:53 +0700

    commit module admisi

--- app/Helpers/MenuHelper.php
+++ app/Helpers/MenuHelper.php
@@ -10,10 +10,18 @@ class MenuHelper
 {
     public static function Menu()
     {
-        $menu_roles = DB::table('role_has_menus')->where('role_id', auth()->user()->id)->get();
+        $menu_roles = DB::table('role_has_menus')->where('role_id', auth()->user()->roles->pluck("id")->first())->get();
+
         foreach ($menu_roles as  $value) {
             $array_menu_roles[] = $value->menu_id;
         }
+        if (!is_null($menu_roles)) {
+            $array_menu_roles = $array_menu_roles;
+        } else {
+            $array_menu_roles = ['user-admisi'];
+        }
+
+
         $menus = Menu::where('parent_id', 0)
             ->with('submenus', function ($query) use ($array_menu_roles) {
                 $query->whereIn('id', $array_menu_roles);
