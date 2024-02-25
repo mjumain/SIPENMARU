2265b9c85ebfe2aa43e424ac3b871f2768253c0a
unknown <mjumain11@gmail.com>
2024-02-25 08:14:52 +0700

    update module admisi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
             </ul>
 
             <ul class="navbar-nav ml-auto ">
-                <li class="nav-item dropdown">
+                {{-- <li class="nav-item dropdown">
                     <a class="nav-link" data-toggle="dropdown" href="#">
                         <i class="far fa-bell"></i>
                         <span class="badge badge-warning navbar-badge">15</span>
@@ -54,7 +54,7 @@
                         <div class="dropdown-divider"></div>
                         <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
                     </div>
-                </li>
+                </li> --}}
                 <li class="nav-item dropdown user-menu">
                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
                         <img src="../../dist/img/user2-160x160.jpg" class="user-image img-circle elevation-2"
